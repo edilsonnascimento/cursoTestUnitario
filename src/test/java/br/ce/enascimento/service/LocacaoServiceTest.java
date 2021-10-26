eeb91ab7dad1aba9eeaad96f6d460ea4fe21fb68
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-10-26 08:39:19 -0300

    07 - Trabalhand com Rules, para mostrar todas as falhas do teste.

--- src/test/java/br/ce/enascimento/service/LocacaoServiceTest.java
+++ src/test/java/br/ce/enascimento/service/LocacaoServiceTest.java
@@ -4,18 +4,22 @@ import br.ce.enascimento.entidades.Filme;
 import br.ce.enascimento.entidades.Locacao;
 import br.ce.enascimento.entidades.Usuario;
 import br.ce.enascimento.utils.DataUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class LocacaoServiceTest {
 
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
     @Test
-    public void main() {
+    public void testarLocacao() {
         //cenario
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Nome do Usuario");
@@ -25,8 +29,8 @@ public class LocacaoServiceTest {
         Locacao locacao = service.alugarFilme(usuario,filme);
 
         //verificacao
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+        error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
     }
 }
